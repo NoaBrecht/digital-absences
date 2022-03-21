5bad3e23efb391fa12eb76cd9de070ce823e747c
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2022-03-21 15:46:47 +0100

    Add files via upload

---
+++ components/header.php
@@ -0,0 +1,57 @@
+<header>
+    <input type="checkbox" id="nav-checkbox" style="display: none;">
+    <div class="menu-content-wrapper">
+        <label for="nav-checkbox">
+            <i class="uil uil-bars" id="menuicon"></i> 
+        </label>
+        <img id="logo-responsive" src="img/digitalabsences-logo_color.png" alt="">
+        <div class="account">
+            <div class="profile-picture-wrapper">
+                <img src="uploads/profilepictures/stijnwillekens.png" alt="">
+            </div>
+            <p>Stijn Willekens</p>
+  <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+  </button>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+    <li><a class="dropdown-item" href="#"><i class="bi bi-person-circle"></i> Profiel aanpassen</a></li>
+    <li><a class="dropdown-item" href="instellingen.php"><i class="bi bi-gear"></i> Instellingen</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="login.php">Log Out</a></li>
+  </ul>
+</div>
+        </div>
+    </div>
+    <nav class="sidebar">
+        <label for="nav-checkbox">
+            <i class="uil uil-angle-left"></i>
+        </label>
+        <img src="img/digitalabsences-logo-color_poweredby.png" alt="">
+        <ul>
+            <li>
+                <a href="startpagina.php" id="startpagina">Startpagina</a>
+            </li>
+            <li>
+                <a href="posters.php" id="posters">Posters</a>
+            </li>
+            <li>
+                <a href="roosters.php" id="roosters">Roosters</a>
+            </li>
+            <li>
+                <a href="leerkrachten.php" id="leerkrachten">Leerkrachten</a>
+            </li>
+            <li>
+                <a href="vervangingen.php" id="vervangingen">vervangingen</a>
+            </li>
+            <li>
+            <a href="Instellingen.php" id="instellingen">Instellingen</a>
+            </li>
+            
+
+            <!-- Alleen voor admin  -->
+            <li>
+            <a href="gebruikers.php" id="gebruikers">Gebruikers</a>
+            </li>
+            
+        </ul>
+    </nav>
+</header>
